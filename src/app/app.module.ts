b2547c14d4f4a9346a19ee5fa562dcb2f5811418
Sebastian Sierra <ysebasst@gmail.com>
2022-08-01 18:57:22 -0500

    Created HomeComponent and FavoritesComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { FavoritesComponent } from './components/favorites/favorites.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  {
+    path: 'favorites',
+    component: FavoritesComponent,
+  },
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, HomeComponent, FavoritesComponent],
+  imports: [RouterModule.forRoot(routes), BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
