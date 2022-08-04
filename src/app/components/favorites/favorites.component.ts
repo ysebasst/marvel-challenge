44491c8470ca28f53158f280d571ee9c8ca86488
Sebastian Sierra <ysebasst@gmail.com>
2022-08-04 03:20:32 -0500

    Created favorites component

--- src/app/components/favorites/favorites.component.ts
+++ src/app/components/favorites/favorites.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { ComicService } from 'src/app/services/comic.service';
 
 @Component({
   selector: 'app-favorites',
   templateUrl: './favorites.component.html',
-  styleUrls: ['./favorites.component.scss']
+  styleUrls: ['./favorites.component.scss'],
 })
 export class FavoritesComponent implements OnInit {
+  favorites: Array<any>;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private comicService: ComicService) {
+    this.favorites = [];
   }
 
+  ngOnInit(): void {
+    this.comicService.getFavorites();
+    this.comicService.favorites$.subscribe((favorites: Array<any>) => {
+      this.favorites = favorites;
+    });
+  }
+
+  getComic(id: number) {
+    this.comicService.getById(id).subscribe((result: any) => {
+      this.comicService.comic.next(result?.data?.results[0]);
+      this.comicService.showModal.next(true);
+    });
+  }
+
+  deleteFavorite(id: any) {
+    this.comicService.deleteFavorite(id);
+  }
 }
